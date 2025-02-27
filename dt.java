ba4c48a0279f81bff1c9be8d85e5f566d46e8586
yasinn187 <148906588+yasinn187@users.noreply.github.com>
2025-02-27 13:06:45 +0300

    Add files via upload

---
+++ dt.java
@@ -0,0 +1,13 @@
+public class dt {
+   public static void main(String[] args) {
+    int grade = 1;
+    double grade_double = 1.5;
+
+    char first = "A";
+    boolean isok = true;
+    System.out.println(grade);
+    System.out.println(grade_double);
+    System.out.println(first);
+    System.out.println(isok);
+   } 
+}
